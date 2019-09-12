c7630b94b3945d845da85d562d24a7c1f8a82a7d
Laura Julià Melis <laaurajulia@gmail.com>
2019-09-12 11:19:13 +0200

    committing important changes

--- awesome_lab3/R/euclidean.R
+++ awesome_lab3/R/euclidean.R
@@ -1,3 +1,20 @@
+#' Euclidean Algorithm
+#'
+#' Euclidean algorithm finds the greatest common divisor between scalars \code{a} and \code{b}.
+#' The algorithm works by... #TODO# 
+#'
+#' @param a scalar
+#' @param b scalar
+#'
+#' @return Scalar value of the greatest common divisor.
+#'
+#' @examples
+#' euclidean(10, 100)
+#' 
+#' @references \url{https://en.wikipedia.org/wiki/Euclidean_algorithm}
+#'
+#' @export
+
 euclidean <- function(a,b){
   stopifnot(is.numeric(a), is.numeric(b))
   k = 2
